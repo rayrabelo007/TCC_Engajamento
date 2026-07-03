dd8f563ff5a8194d240f10701c3c11fb9c4d239f
rayrabelo007 <raissarabelo74@gmail.com>
2026-07-03 00:23:48 -0400

    Run successful pipeline + RAG + ML metrics update

--- src/config.py
+++ src/config.py
@@ -8,12 +8,14 @@ YOUTUBE_API_KEY: str = os.environ.get("YOUTUBE_API_KEY", "")
 
 # Coloque aqui os IDs dos vídeos que você quer analisar para o seu TCC
 VIDEO_IDS: List[str] = [
-    "dQw4w9WgXcQ",  # Exemplo 1 (Substitua pelos IDs reais dos anúncios/reviews)
-    "jNQXAC9IVRw",  # Exemplo 2
+    "HhesaQXLuRY",
+    "5MgBikgcWnY",
+    "aircAruvnKk",
+    "2ePf9rue1Ao",
 ]
 
 # Modelo ideal para múltiplos idiomas incluindo Português
-SENTIMENT_MODEL_NAME: str = "finiteautomata/bertwithsmiles-portuguese-tweets"
+SENTIMENT_MODEL_NAME: str = "pysentimiento/bertweet-pt-sentiment"
 
 MAX_COMMENTS_PER_VIDEO: int = 100
 MIN_CLEANED_TEXT_LENGTH: int = 3
